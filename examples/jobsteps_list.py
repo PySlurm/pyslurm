cb710d531ee81f0e232858943660a77edf396dad
gingergeeks <mark@gingergeeks.co.uk>
2011-12-20 12:55:55 +0000

    Modified to use new jobstep class

--- examples/jobsteps_list.py
+++ examples/jobsteps_list.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python
+
 import pyslurm
+import sys
 from time import gmtime, strftime
 
-a = pyslurm.slurm_get_job_steps(5,0,0)
+steps = pyslurm.jobstep()
+a = steps.get()
+
 for job, job_step in sorted(a.iteritems()):
+
 
 		print "Job: %s" % (job)
 		for step, step_data in sorted(job_step.iteritems()):
@@ -12,9 +18,14 @@ for job, job_step in sorted(a.iteritems()):
 
 				if 'start_time' in step_item:
 					ddate = pyslurm.epoch2date(item_data)
-					print "\t\t%-10s : %s" % (step_item, ddate)
+					print "\t\t%-15s : %s" % (step_item, ddate)
 				else:
-					print "\t\t%-10s : %s" % (step_item, item_data)
+					print "\t\t%-15s : %s" % (step_item, item_data)
+
+			layout = steps.layout(job, step)
+			print "\t\tLayout:"
+			for name, value in sorted(layout.iteritems()):
+				print "\t\t\t%-15s : %s" % (name, value)
 
 print "-" * 80
 
