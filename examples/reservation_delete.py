07274a4ba6b0935ea695c37c758f23e1321807b3
gingergeeks <mark@gingergeeks.co.uk>
2012-07-02 20:12:09 +0100

    Ongoing API test

--- examples/reservation_delete.py
+++ examples/reservation_delete.py
@@ -2,6 +2,6 @@
 
 import pyslurm
 
-rc = pyslurm.slurm_delete_reservation("root_12")
+rc = pyslurm.slurm_delete_reservation("root_18")
 if rc != 0:
 	print "Error : %s" % pyslurm.slurm_strerror(pyslurm.slurm_get_errno())
