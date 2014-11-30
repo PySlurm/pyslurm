759aca3c8391afd8d3682e0a58d7eb07d0f3fb11
gingergeeks <mark@gingergeeks.co.uk>
2014-11-30 11:58:33 +0000

    Modified to take single jobid

--- examples/cancel_job.py
+++ examples/cancel_job.py
@@ -2,7 +2,7 @@
 
 import pyslurm
 
-rc = pyslurm.slurm_kill_job(51, 9, 0)
+rc = pyslurm.slurm_kill_job(26)
 if rc == -1:
 	print "Error : %s" % pyslurm.slurm_strerror(pyslurm.slurm_get_errno())
 else:
