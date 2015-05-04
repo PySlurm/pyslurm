54b3cb89bd7ea52e2cb44d1756b2637e58be52ae
gingergeeks <mark@gingergeeks.co.uk>
2015-05-04 20:48:27 +0100

    Updated to reflect version

--- pyslurm/__init__.py
+++ pyslurm/__init__.py
@@ -20,6 +20,6 @@ if old_dlopen_flags:
 	if hasattr(sys, "setdlopenflags"):
 		sys.setdlopenflags(old_dlopen_flags)
 
-__version__ = "14.11.0-0"
+__version__ = "14.11.0-5"
 def version():
 	return __version__
