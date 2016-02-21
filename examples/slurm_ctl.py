c3b0cbfa8d26023d6fae9eaa5f6c91079e1d5f46
gingergeeks <mark@gingergeeks.co.uk>
2016-02-21 15:37:36 +0000

    Removed debug code

--- examples/slurm_ctl.py
+++ examples/slurm_ctl.py
@@ -5,12 +5,9 @@ from __future__ import print_function
 import pyslurm
 import sys
 
-a = pyslurm.config()
-a.get()
 try:
 	a = pyslurm.config()
 	ctl_dict = a.get()
-	print("%s" % ctl_dict)
 except ValueError as e:
 	print("Error - {0}".format(e.args[0]))
 	sys.exit(-1)
