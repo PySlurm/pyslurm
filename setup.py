b899ab650e2608da4b15b423acbcd6dee47ef469
giovtorres <giovanni.torres@gmail.com>
2016-11-05 11:35:01 -0400

    Update versions

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ logging.basicConfig(level=20)
 # PySlurm Version
 
 #VERSION = imp.load_source("/tmp", "pyslurm/__init__.py").__version__
-__version__ = "16.05.0"
-__min_slurm_hex_version__ = "0x100500"
+__version__ = "16.05.5"
+__min_slurm_hex_version__ = "0x100505"
 __max_slurm_hex_version__ = "0x100505"
 
 def fatal(logstring, code=1):
