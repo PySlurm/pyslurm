128642bf127264022eaec3b7844cd6addf7ed44f
Giovanni Torres <giovanni.torres@gmail.com>
2016-11-21 19:39:25 -0500

    Added tests for hostlist class

---
+++ tests/test-hostlist.py
@@ -0,0 +1,43 @@
+from __future__ import absolute_import, unicode_literals
+
+import pyslurm
+from nose.tools import assert_equals, assert_true
+
+def test_hostlist_create_new():
+    """Hostlist: Create new hostlist"""
+    hl = pyslurm.hostlist.Hostlist()
+    hosts = "c1, c[2-3]"
+
+    assert_true(hl.create(hosts))
+    assert_equals(hl.count(), 3)
+
+    assert_equals(hl.find("c3"), 2)
+
+    assert_equals(hl.push("c[4-5]"), 2)
+    assert_equals(hl.count(), 5)
+
+    assert_equals(hl.push_host("c6"), 1)
+
+    assert_equals(hl.ranged_string(), "c[1-6]")
+
+    assert_equals(hl.shift(), "c1")
+    assert_equals(hl.count(), 5)
+
+    assert_equals(hl.push_host("c6"), 1)
+    assert_equals(hl.ranged_string(), "c[2-6,6]")
+
+    hl.uniq()
+    assert_equals(hl.ranged_string(), "c[2-6]")
+
+    hl.destroy()
+    assert_equals(hl.count(), -1)
+
+
+def test_hostlist_create_empty():
+    hl = pyslurm.hostlist.Hostlist()
+
+    hl.create()
+    assert_equals(hl.count(), 0)
+
+    hl.destroy()
+    assert_equals(hl.count(), -1)
