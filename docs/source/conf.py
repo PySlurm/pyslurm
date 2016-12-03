408df627ec1e68ac9a562e268e62a40570d340b7
Giovanni Torres <giovanni.torres@gmail.com>
2016-12-03 18:39:34 -0500

    Try to mock modules for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,20 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
-# import sys
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 from datetime import datetime
+from unittest.mock import MagicMock
+
+# -- Mock imports for PySlurm ---------------------------------------------
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ['pyslurm']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
