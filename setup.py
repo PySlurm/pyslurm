17eae141c4ab8f1d3fa1b46ba232c24f0c6acfd7
Giovanni Torres <giovanni.torres@gmail.com>
2017-05-19 08:51:51 -0400

    Added slurm to the libraries extension list

--- setup.py
+++ setup.py
@@ -68,8 +68,8 @@ def makeExtension(extName):
         extName,
         [extPath],
         include_dirs = ['%s' % SLURM_INC, '.'],   # adding the '.' to include_dirs is CRUCIAL!!
-        library_dirs = ['%s' % SLURM_LIB, '%s/slurm' % SLURM_LIB], 
-        libraries = ['slurmdb'],
+        library_dirs = ['%s' % SLURM_LIB, '%s/slurm' % SLURM_LIB],
+        libraries = ['slurmdb', 'slurm'],
         runtime_library_dirs = ['%s/' % SLURM_LIB, '%s/slurm' % SLURM_LIB],
         extra_objects = [],
         extra_compile_args = [],
