621d573204273da922e814b8c392041b5a1ee157
Giovanni Torres <giovanni.torres@gmail.com>
2017-07-14 16:49:08 -0400

    Removed cython==0.15.1 with python==3.4 from travis builds

--- setup.py
+++ setup.py
@@ -326,9 +326,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
