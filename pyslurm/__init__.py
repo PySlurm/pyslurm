937f48c37074b9849af59edcb9f760c78a2ecc40
Giovanni Torres <giovanni.torres@gmail.com>
2018-03-02 14:58:01 -0500

    Also moved long_description to setup_package()
    - Bump version to 17.11.0.6

--- pyslurm/__init__.py
+++ pyslurm/__init__.py
@@ -11,7 +11,7 @@ from __future__ import absolute_import
 import ctypes
 import sys
 
-__version__ = "17.11.0.5"
+__version__ = "17.11.0.6"
 
 sys.setdlopenflags(sys.getdlopenflags() | ctypes.RTLD_GLOBAL)
 
