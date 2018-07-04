588b369e6f72b6df53f0ba5ca116c0060e7221c7
Giovanni Torres <giovtorres@users.noreply.github.com>
2018-07-04 09:56:23 -0400

    Update CI scripts. (#118)
    * Combine config and debug scripts
    * Replace non-deterministic sleep waits with improved until statements
    * Add some echo statements for debugging in Travis console
    * Updated .travis.yml with new scripts
    * Added squeue output
    * Replace tabs with spaces
    * Fix topology.conf.  c0 is invalid.  Changed to c1.
    * Refactored duplicate code into wait_for_cmd()
    * Added waits for checking licenses and topology configuration
    * Restart mysqld and slurmdbd if it does not start

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 set -e
 
-#
-# Build the project.
-#
+###################################
+# Install Python and Build PySlurm
+###################################
 
 # Vars and functions for installing Python 3.5 and 3.6 via IUS repo.
 RELEASE_RPM=$(rpm -qf /etc/redhat-release)
 RELEASE=$(rpm -q --qf '%{VERSION}' ${RELEASE_RPM})
 
 import_ius_key(){
-	rpm --import /etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY
+    rpm --import /etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY
 }
 
 centos_install_ius(){
-	case ${RELEASE} in
-		6*) yum -y install https://centos6.iuscommunity.org/ius-release.rpm;;
-		7*) yum -y install https://centos7.iuscommunity.org/ius-release.rpm;;
-	esac
-	import_ius_key
+    case ${RELEASE} in
+        6*) yum -y install https://centos6.iuscommunity.org/ius-release.rpm;;
+        7*) yum -y install https://centos7.iuscommunity.org/ius-release.rpm;;
+    esac
+    import_ius_key
 }
 
-# Install Python
+# NOTE: Python is already pre-installed in the later containers
+
+# Install Python versions from YUM repositories
 if [ "$PYTHON" == "3.4" ]; then
     yum makecache fast && yum -y install python34{,-devel,-pip}
 fi
@@ -48,18 +50,21 @@ fi
 
 if [ "$PYTHON" == "2.6" ]
 then
+    echo "---> Upgrading setuptools to 36.8.0..."
     pip install --upgrade setuptools==36.8.0
 fi
 
 # Upgrade pip
+echo "---> Upgrading pip to 9.0.3..."
 pip install --upgrade pip==9.0.3
 
 # Install nose
+echo "---> Installing nose and Cython pip packages..."
 pip$PYTHON install nose Cython==$CYTHON
 
 cd pyslurm
-echo "Building PySlurm..."
+echo "---> Building PySlurm..."
 python$PYTHON setup.py build
 
-echo "Installing PySlurm..."
+echo "---> Installing PySlurm..."
 python$PYTHON setup.py install
