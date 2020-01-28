f664c6ce83e460d7cb2cc87a11a56e12ef49a316
Chris Hines <chris.hines@monash.edu.au>
2020-01-28 14:04:43 +1100

    code formating and doc strings on test functions

--- tests/test-slurmdb.py
+++ tests/test-slurmdb.py
@@ -1,54 +1,81 @@
-from __future__ import absolute_import, unicode_literals
+import datetime
+import pwd
+import subprocess
+
+from nose.tools import assert_equals
 
 import pyslurm
-import subprocess
-from nose.tools import assert_equals, assert_true
-from socket import gethostname
-import datetime
 
-def njobs_sacct_jobs(start, end):
-    sacct = subprocess.Popen(['sacct','-S',start,'-E',end,'-n','-X','-a'],stdout=subprocess.PIPE,stderr=None).communicate()
+
+def njobs_sacct_jobs(start, end, username=None):
+    """
+    Count the number of jobs reported by sacct
+    For comparison with the reults of slurmdb_jobs.get
+    """
+    sacctcmd = ['sacct', '-S', start, '-E', end, '-n', '-X']
+    if username is not None:
+        sacctcmd.extend(['-u', username])
+    else:
+        sacctcmd.append('-a')
+    sacct = subprocess.Popen(sacctcmd,
+                             stdout=subprocess.PIPE,
+                             stderr=None).communicate()
     return len(sacct[0].splitlines())
 
-def njobs_sacct_jobs_byuser(start, end, username):
-    sacct = subprocess.Popen(['sacct','-S',start,'-E',end,'-n','-X','-u',username],stdout=subprocess.PIPE,stderr=None).communicate()
-    return len(sacct[0].splitlines())
 
-def njobs_slurmdb_jobs_get(start,end):
-    jobs = pyslurm.slurmdb_jobs().get(starttime=start.encode('utf-8'), endtime=end.encode('utf-8'))
+def njobs_slurmdb_jobs_get(start, end, uid=None):
+    """
+    Count the number of jobs reported by slurmdb
+    """
+    if uid is None:
+        jobs = pyslurm.slurmdb_jobs().get(starttime=start.encode('utf-8'),
+                                          endtime=end.encode('utf-8'))
+    else:
+        jobs = pyslurm.slurmdb_jobs().get(starttime=start.encode('utf-8'),
+                                          endtime=end.encode('utf-8'),
+                                          userids=[uid])
     return len(jobs)
 
-def njobs_slurmdb_jobs_get_byuid(start,end,uid):
-    print(uid)
-    jobs = pyslurm.slurmdb_jobs().get(starttime=start.encode('utf-8'), endtime=end.encode('utf-8'),userids=[uid])
-    print('njobs by py slumr {}'.format(len(jobs)))
-    return len(jobs)
 
 def get_user():
-    import pwd
-    users = subprocess.Popen(['squeue', '-O', 'username', '-h'],stdout=subprocess.PIPE,stderr=None).communicate()
+    """
+    Return a list of usernames and their uid numbers
+    """
+    users = subprocess.Popen(['squeue', '-O', 'username', '-h'],
+                             stdout=subprocess.PIPE,
+                             stderr=None).communicate()
     for username in users[0].splitlines():
         print(username.decode())
         uid = pwd.getpwnam("{}".format(username.strip().decode()))
-        yield username.strip().decode(),uid.pw_uid
+        yield username.strip().decode(), uid.pw_uid
 
-    
 
 def test_slurmdb_jobs_get():
-    starttime = (datetime.datetime.now()-datetime.timedelta(days=2)).strftime("%Y-%m-%dT00:00:00")
-    endtime = (datetime.datetime.now()-datetime.timedelta(days=1)).strftime("%Y-%m-%dT00:00:00")
-    njobs_pyslurm = njobs_slurmdb_jobs_get(starttime,endtime)
-    njobs_sacct = njobs_sacct_jobs(starttime,endtime)
-    assert_equals(njobs_pyslurm,njobs_sacct)
+    """
+    Slurmdb: Compare sacct and slurmdb_jobs.get() for all users
+    """
+    starttime = (datetime.datetime.now() -
+                 datetime.timedelta(days=2)).strftime("%Y-%m-%dT00:00:00")
+    endtime = (datetime.datetime.now() -
+               datetime.timedelta(days=1)).strftime("%Y-%m-%dT00:00:00")
+    njobs_pyslurm = njobs_slurmdb_jobs_get(starttime, endtime)
+    njobs_sacct = njobs_sacct_jobs(starttime, endtime)
+    assert_equals(njobs_pyslurm, njobs_sacct)
+
 
 def test_slurmdb_jobs_get_byuser():
+    """
+    Slurmdb: Compare sacct and slurmdb_jobs.get() for individual users
+    """
+
     userlist = list(get_user())
     for user in userlist[:10]:
-        starttime = (datetime.datetime.now()-datetime.timedelta(days=2)).strftime("%Y-%m-%dT00:00:00")
-        endtime = (datetime.datetime.now()-datetime.timedelta(days=1)).strftime("%Y-%m-%dT00:00:00")
-        njobs_pyslurm = njobs_slurmdb_jobs_get_byuid(starttime,endtime,int(user[1])) 
-        njobs_sacct = njobs_sacct_jobs_byuser(starttime,endtime,user[0])
-        print('njobs by sacct {}'.format(njobs_sacct))
-        assert_equals(njobs_pyslurm,njobs_sacct)
-    
-
+        starttime = (datetime.datetime.now() -
+                     datetime.timedelta(days=2)).strftime("%Y-%m-%dT00:00:00")
+        endtime = (datetime.datetime.now() -
+                   datetime.timedelta(days=1)).strftime("%Y-%m-%dT00:00:00")
+        njobs_pyslurm = njobs_slurmdb_jobs_get(starttime,
+                                               endtime,
+                                               int(user[1]))
+        njobs_sacct = njobs_sacct_jobs(starttime, endtime, username=user[0])
+        assert_equals(njobs_pyslurm, njobs_sacct)
