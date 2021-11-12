22b35474a2608448e36d9e6f83b13ea9ecb46084
Giovanni Torres <giovtorres@users.noreply.github.com>
2021-11-12 18:23:23 -0500

    feat: add github workflow (#196)
    This change replaces TravisCI with GitHub Actions for running CI tests.

--- scripts/run_tests_in_container.py
+++ scripts/run_tests_in_container.py
@@ -1,9 +1,16 @@
+"""Execute pytest inside the container."""
+
+import os
+
 import testinfra
 
 
 def test_run():
     host = testinfra.get_host(f"docker://slurmctl")
-    print(host.check_output("python3.9 setup.py build"))
-    print(host.check_output("python3.9 setup.py install"))
+    python = f'python{os.environ.get("PYTHON")}'
+    print(host.check_output(f"{python} setup.py build"))
+    print(host.check_output(f"{python} setup.py install"))
     print(host.check_output("./scripts/configure.sh"))
+    print(host.check_output(f"{python} -m pip uninstall --yes pytest"))
+    print(host.check_output(f"{python} -m pip install pytest"))
     print(host.check_output("pytest -v"))
