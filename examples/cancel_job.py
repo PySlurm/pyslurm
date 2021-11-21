152cd191928dafc355631d0ae430b2fe81b02f40
gingergeeks <mark@gingergeeks.co.uk>
2021-11-21 15:20:35 +0000

    style: autopep8/black recommendations, f-strings, code cleanup

--- examples/cancel_job.py
+++ examples/cancel_job.py
@@ -8,6 +8,6 @@ import pyslurm
 try:
     rc = pyslurm.slurm_kill_job(51, 9, 0)
 except ValueError as value_error:
-    print("Cancel job error - {0}".format(value_error.args[0]))
+    print(b"Cancel job error - {value_error.args[0]}")
 else:
     print("Success - cancelled job")
