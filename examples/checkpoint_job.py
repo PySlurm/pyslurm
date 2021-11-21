2adf917467b303c71213aa474010f00be65d91d4
gingergeeks <mark@gingergeeks.co.uk>
2021-11-21 15:20:44 +0000

    style: autopep8/black recommendations, f-strings, code cleanup

--- examples/checkpoint_job.py
+++ examples/checkpoint_job.py
@@ -8,6 +8,6 @@ import pyslurm
 try:
     time = pyslurm.slurm_checkpoint_able(2, 0)
 except ValueError as value_error:
-    print("Checkpointable job failed - {0}".format(value_error.args[0]))
+    print(f"Checkpointable job failed - {value_error.args[0]}")
 else:
-    print("Job can be checkpointed at {0}".format(time))
+    print(f"Job can be checkpointed at {format(time)}")
