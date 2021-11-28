a8a5fd9314e490a5745ebac6098dfd57613098e0
Giovanni Torres <giovanni.torres@gmail.com>
2021-11-27 21:01:50 -0500

    feat: support 3.10.0

--- scripts/run_tests_in_container.py
+++ scripts/run_tests_in_container.py
@@ -1,16 +1,19 @@
 """Execute pytest inside the container."""
 
 import os
+import sys
 
 import testinfra
 
 
 def test_run():
     host = testinfra.get_host(f"docker://slurmctl")
-    python = f'python{os.environ.get("PYTHON")}'
-    print(host.check_output(f"{python} setup.py build"))
-    print(host.check_output(f"{python} setup.py install"))
+    python_full = os.environ.get("PYTHON")
+    python_short = ".".join([str(sys.version_info.major), str(sys.version_info.minor)])
+    print(host.check_output(f"pyenv global {python_full}"))
+    print(host.check_output(f"{python_short} setup.py build"))
+    print(host.check_output(f"{python_short} setup.py install"))
     print(host.check_output("./scripts/configure.sh"))
-    print(host.check_output(f"{python} -m pip uninstall --yes pytest"))
-    print(host.check_output(f"{python} -m pip install pytest"))
+    print(host.check_output(f"{python_short} -m pip uninstall --yes pytest"))
+    print(host.check_output(f"{python_short} -m pip install pytest"))
     print(host.check_output("pytest -v"))
