61d4740c4c2e812e4983ab9558d1d23060b93c7f
wresch <wresch@drgang.net>
2023-02-09 15:04:59 -0500

    Brought __rpc_num2string in sync with slurm 22.05 message types (#261)

---
+++ scripts/slurm_msg_type_dict.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+"""
+Parse $slurmrepo/src/common/slurm_protocol_defs.h and create
+a small C program that generates a mapping of the numeric
+slurm msg types to their symbolic names.
+
+Example:
+    ./slurm_msg_type_dict.py $slurmrepo/src/common/slurm_protocol_defs.h > msgdict.c
+    gcc -o msgdict msgdict.c
+    ./msgdict
+"""
+
+import re
+import sys
+import argparse
+
+def generate_c(header_file_name):
+    typedef_re = re.compile(r"\s*typedef\s+enum\s*{(.*?)}\s*slurm_msg_type_t\s*;", re.DOTALL)
+    symbol_re = re.compile(r"^\s*([A-Z0-9_]+)\s*[,=\n]")
+
+    with open(header_file_name, mode="r", encoding="utf-8") as header_file:
+        header = header_file.read()
+    typedef = typedef_re.search(header)
+    if typedef is None:
+        print("could not identify the slurm_msg_type_t typedef in the header file")
+        sys.exit(1)
+
+    print("""#include <stdio.h>""")
+    print(typedef.group(0))
+    print("""\n\nint main(void) {""")
+    for line in typedef.group(1).split("\n"):
+        symbol = symbol_re.match(line)
+        if symbol is not None:
+            print(f"""    printf("%d: \\\"%s\\\",\\n", {symbol.group(1)}, "{symbol.group(1)}");""")
+        else:
+            print(f"""    printf("\\n");""")
+    print("""    return 0;\n}""")
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("header", help="$slurmrepo/src/common/slurm_protocol_defs.h")
+    args = parser.parse_args()
+    generate_c(args.header)
+
+if __name__ == "__main__":
+    main()
