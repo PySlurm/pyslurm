f098efb1217413a7ea0fb9e2f338bb635176c596
tazend <75485188+tazend@users.noreply.github.com>
2023-12-11 22:25:22 +0100

    Improve setup.py a bit (#328)

--- pyslurm/__version__.py
+++ pyslurm/__version__.py
@@ -1,1 +1,8 @@
+# Version is in the format: SLURM_MAJOR.PYSLURM_PATCH
+#
+# The first two Numbers "X.Y" represent the current Slurm Major release that
+# this Version of PySlurm is based on.
+# The last Number "Z" is the current Pyslurm patch version, which should be
+# incremented each time a new release is made (except when migrating to a new
+# Slurm Major release, then set it back to 0)
 __version__ = "23.2.2"
