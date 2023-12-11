7e094013cbd05ec4b68ac26bc51d0cf5a95b6152
tazend <toni.harzendorf@gmail.com>
2023-12-11 23:14:03 +0100

    upgrade: bump version

--- pyslurm/__version__.py
+++ pyslurm/__version__.py
@@ -5,4 +5,4 @@
 # The last Number "Z" is the current Pyslurm patch version, which should be
 # incremented each time a new release is made (except when migrating to a new
 # Slurm Major release, then set it back to 0)
-__version__ = "23.2.2"
+__version__ = "23.11.0"
