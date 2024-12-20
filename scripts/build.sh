11ae386eecb0bac582377a78a57dd99c1b549a1c
Toni Harzendorf <toni.harzendorf@ufz.de>
2024-12-20 17:39:52 +0100

    build.sh: use pip install

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,7 @@ set -e
 
 usage() { echo "Usage: $0 [-j jobs]" 1>&2; exit 1; }
 
-# Option to allow parallel build
-OPT_JOBS=1
-
-PYTHON_VERSION=3
+OPT_JOBS=${PYSLURM_BUILD_JOBS:-1}
 
 while getopts ":j:" o; do
     case "${o}" in
@@ -21,5 +18,15 @@ done
 
 shift $((OPTIND-1))
 
-python"$PYTHON_VERSION" setup.py build -j "$OPT_JOBS"
-python"$PYTHON_VERSION" setup.py install
+PY_VER=$(python -c "import sys; v=sys.version_info; print(f'{v.major}.{v.minor}')")
+echo "Building with ${OPT_JOBS} cores"
+export PYSLURM_BUILD_JOBS="$OPT_JOBS"
+
+if [[ $PY_VER == "3.6" ]]
+then
+    pip install -v .
+else
+    pip install -v . --config-settings="--build-option=build_ext -j${OPT_JOBS}"
+fi
+
+
