e4444c520128c809cf85711585984e1b27466478
tazend <75485188+tazend@users.noreply.github.com>
2024-12-30 14:24:28 +0100

    tests: remove old API tests under tests/ directory directly (#363)
    Most of them do either not work correctly anymore (and perhaps only
    specifically in the context of the slurm docker containers, which are outdated), or they do
    not really test anything substantial (like the test_qos.py file)
    
    Tests for the new API are in tests/unit and tests/integration

--- tests/test_qos.py
+++
@@ -1,9 +0,0 @@
-"""Test cases for QoS."""
-
-import pyslurm
-
-
-def test_get_qos():
-    """QoS: Test get_qos() return type"""
-    test_qos = pyslurm.qos().get()
-    assert isinstance(test_qos, dict)
--- tests/test_statistics.py
+++
@@ -1,9 +0,0 @@
-"""Test cases for Slurm statistics."""
-
-import pyslurm
-
-
-def test_get_statistics():
-    """Statistics: Test get_statistics() return type"""
-    test_statistics = pyslurm.statistics().get()
-    assert isinstance(test_statistics, dict)
