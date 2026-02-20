aff96e2c5697e25c03a85b579079de5e075af952
Toni Harzendorf <toni.harzendorf@gmail.com>
2026-02-20 11:29:23 +0100

    rework tres/gres API classes
    - also add new GPU and GenericResourceLayout Types

--- pyslurm/db/tres.pxd
+++ pyslurm/db/tres.pxd
@@ -42,14 +42,92 @@ from pyslurm.db.connection cimport Connection
 cdef find_tres_count(char *tres_str, typ, on_noval=*, on_inf=*)
 cdef find_tres_limit(char *tres_str, typ)
 cdef merge_tres_str(char **tres_str, typ, val)
-cdef _tres_ids_to_names(char *tres_str, TrackableResources tres_data)
-cdef _set_tres_limits(char **dest, TrackableResourceLimits src,
-                          TrackableResources tres_data)
+cdef _tres_ids_to_names(char *tres_str, dict tres_id_map)
+cdef _set_tres_limits(char **dest, src, tres_data)
 
 
-cdef class TrackableResourceLimits:
+cdef class FilesystemResources(dict):
+    """Collection of Filesystem TRES. This inherits from `dict`."""
+    pass
+
+
+cdef class GenericResources(dict):
+    """Collection of Generic TRES. This inherits from `dict`."""
+    pass
+
+
+cdef class InterconnectResources(dict):
+    """Collection of Interconnect TRES. This inherits from `dict`."""
+    pass
+
+
+cdef class LicenseResources(dict):
+    """Collection of License TRES. This inherits from `dict`."""
+    pass
+
+
+cdef class OtherResources(dict):
+    """Collection of Other TRES. This inherits from `dict`."""
+    pass
+
+
+
+cdef class GenericResourceLayout:
 
     cdef public:
+        name
+        type
+        count
+        indexes
+
+
+cdef class TrackableResourceFilter:
+    cdef slurmdb_tres_cond_t *ptr
+
+
+cdef class TrackableResources:
+    """Trackable Resources in the Slurm Database.
+
+    Args:
+        global_tres_data (Union[dict, TrackableResources], optional=None):
+            This is only required when specifying TRES with their numeric ID,
+            so they can be properly translated.
+        **kwargs (Any, optional=None):
+            Any valid attribute of the object.
+
+    Attributes:
+        cpu (pyslurm.db.TrackableResource):
+            CPU TRES
+        mem (pyslurm.db.TrackableResource):
+            Mem TRES
+        energy (pyslurm.db.TrackableResource):
+            Energy TRES
+        node (pyslurm.db.TrackableResource):
+            Node TRES
+        billing (pyslurm.db.TrackableResource):
+            Billing TRES
+        vmem (pyslurm.db.TrackableResource):
+            VMem TRES
+        pages (pyslurm.db.TrackableResource):
+            Pages TRES
+        fs (pyslurm.db.tres.FilesystemResources):
+            Filesystem Resources
+        gres (pyslurm.db.tres.GenericResources):
+            Generic Resources
+        license (pyslurm.db.tres.LicenseResources):
+            License Resources
+        interconnect (pyslurm.db.tres.InterconnectResources):
+            Interconnect Resources
+        other (pyslurm.db.tres.OtherResources):
+            Other Resources
+            Here are all Resources that are not built-in TRES-Types, when the
+            Slurm source code has been modified to add a custom TRES Type
+    """
+    cdef public:
+        dict _id_map
+
+    cdef public:
+        raw_str
         cpu
         mem
         energy
@@ -60,27 +138,78 @@ cdef class TrackableResourceLimits:
         pages
         gres
         license
+        interconnect
+        other
 
     @staticmethod
-    cdef from_ids(char *tres_id_str, TrackableResources tres_data)
-
-
-cdef class TrackableResourceFilter:
-    cdef slurmdb_tres_cond_t *ptr
-
-
-cdef class TrackableResources(dict):
-    cdef public raw_str
-
-    @staticmethod
-    cdef TrackableResources from_str(char *tres_str)
+    cdef TrackableResources from_cstr(char *tres_str, global_tres_data=*, on_empty=*)
 
     @staticmethod
     cdef find_count_in_str(char *tres_str, typ, on_noval=*, on_inf=*)
 
 
 cdef class TrackableResource:
-    cdef slurmdb_tres_rec_t *ptr  
+    """A Trackable Resource in the Slurm Database.
+
+    Args:
+        tres_type (str):
+            The Type of the TRES
+        count (int):
+            The value/count of the TRES
+        name (str, optional=None):
+            An optional name of the TRES.
+            For example with the "fs/disk" TRES, the name would be "disk", and
+            the type is "fs"
+        tres_id (int, optional=None):
+            The numeric ID of the TRES in the Database.
+
+    Attributes:
+        type (str):
+            The Type of the TRES
+        count (int):
+            The value/count of the TRES
+        name (str):
+            An optional name of the TRES.
+            For example with the "fs/disk" TRES, the name would be "disk", and
+            the type is "fs"
+        id (int):
+            The numeric ID of the TRES in the Database.
+    """
+    cdef public:
+        id
+        name
+        type
+        count
+
 
     @staticmethod
-    cdef TrackableResource from_ptr(slurmdb_tres_rec_t *in_ptr)
+    cdef TrackableResource from_ptr(slurmdb_tres_rec_t *ptr)
+
+
+cdef class GPU:
+    """GPU as a specific subtype for a Trackable Resource.
+
+    Args:
+        count (int):
+            The value/count of the TRES
+        gpu_type (str):
+            A subtype for the GPU, for example "nvidia-a100"
+        tres_id (int, optional=None):
+            The numeric ID of the GPU TRES in the Database.
+
+    Attributes:
+        type (str):
+            The subtype for the GPU
+        count (int):
+            The value/count of the TRES
+        name (str):
+            The name and subtype of the GPU. For example "gpu:nvidia-a100". If
+            there is no subtype, it will just be "gpu"
+        type_and_name (int):
+            The same as `name`, but also include the "gres/" identifier as
+            prefix. For example: "gres/gpu:nvidia-a100"
+    """
+    cdef public:
+        id
+        type
+        count
--- pyslurm/db/tres.pyx
+++ pyslurm/db/tres.pyx
@@ -22,89 +22,123 @@
 # cython: c_string_type=unicode, c_string_encoding=default
 # cython: language_level=3
 
+from typing import Union
 from pyslurm.utils.uint import *
 from pyslurm.constants import UNLIMITED
 from pyslurm.core.error import RPCError
-from pyslurm.utils.helpers import instance_to_dict
+from pyslurm.utils.helpers import instance_to_dict, dehumanize
 from pyslurm.utils import cstr
 from pyslurm.db.connection import _open_conn_or_error
+from pyslurm import xcollections
 import json
+import re
 
 
 TRES_TYPE_DELIM = "/"
+GRES_TYPE_DELIM = "gres:"
+GRES_TYPE_DELIM_2 = "gres/"
+
+TRES_NAME_REQUIRED = ["fs", "license", "interconnect", "gres"]
+
+gres_pattern = re.compile(r'[/:]')
 
 
-cdef class TrackableResourceLimits:
+cdef class FilesystemResources(dict):
 
-    def __init__(self, **kwargs):
-        self.fs = {}
-        self.gres = {}
-        self.license = {}
+    def to_dict(self, recursive=False):
+        return xcollections.dict_recursive(self, recursive)
 
-        for k, v in kwargs.items():
-            if TRES_TYPE_DELIM in k:
-                typ, name = self._unflatten_tres(k)
-                cur_val = getattr(self, typ)
 
-                if not isinstance(cur_val, dict):
-                    raise ValueError(f"TRES Type {typ} cannot have a name "
-                                     f"({name}). Invalid Value: {typ}/{name}")
+cdef class GenericResources(dict):
 
-                cur_val.update({name : int(v)})
-                setattr(self, typ, cur_val)
-            else:
-                setattr(self, k, v)
+    def to_dict(self, recursive=False):
+        return xcollections.dict_recursive(self, recursive)
+
+
+cdef class InterconnectResources(dict):
+
+    def to_dict(self, recursive=False):
+        return xcollections.dict_recursive(self, recursive)
+
+
+cdef class LicenseResources(dict):
+
+    def to_dict(self, recursive=False):
+        return xcollections.dict_recursive(self, recursive)
+
+
+cdef class OtherResources(dict):
+
+    def to_dict(self, recursive=False):
+        return xcollections.dict_recursive(self, recursive)
+
+
+cdef class GenericResourceLayout:
+
+    def __init__(self, name, typ=None, count=1, indexes=None):
+        self.name = name
+        self.type = typ
+        self.count = int(count)
+        self.indexes = [] if not indexes else indexes
+
+    def to_dict(self, recursive=False):
+        return instance_to_dict(self, recursive)
 
     @staticmethod
-    cdef from_ids(char *tres_id_str, TrackableResources tres_data):
-        tres_list = _tres_ids_to_names(tres_id_str, tres_data)
-        if not tres_list:
-            return None
+    def from_str(str gres_str):
+        cdef:
+            dict output = {}
+            GenericResourceLayout gres = None
 
-        cdef TrackableResourceLimits out = TrackableResourceLimits()
+        if not gres_str or gres_str == "(null)":
+            return {}
 
-        for tres in tres_list:
-            typ, name, cnt = tres
-            cur_val = getattr(out, typ, slurm.NO_VAL64)
-            if cur_val != slurm.NO_VAL64:
-                if isinstance(cur_val, dict):
-                    cur_val.update({name : cnt})
-                    setattr(out, typ, cur_val)
-                else:
-                    setattr(out, typ, cnt)
+        for item in re.split(",(?=[^,]+?:)", gres_str):
 
-        return out
+            # char *gres might contain just "gres:gpu", without any count.
+            # If not given, the count is always 1, so default to it.
+            cnt = typ = "1"
 
-    def _validate(self, TrackableResources tres_data):
-        id_dict = _tres_names_to_ids(self.to_dict(flatten_limits=True),
-                                    tres_data)
-        return id_dict
+            # Remove the additional "gres" specifier if it exists
+            if GRES_TYPE_DELIM in item:
+                item = item.replace(GRES_TYPE_DELIM, "")
+            elif GRES_TYPE_DELIM_2 in item:
+                item = item.replace(GRES_TYPE_DELIM_2, "")
 
-    def _unflatten_tres(self, type_and_name):
-        typ, name = type_and_name.split(TRES_TYPE_DELIM, 1)
-        return typ, name
+            gres_splitted = re.split(
+                ":(?=[^:]+?)",
+                item.replace("(", ":", 1).replace(")", "")
+            )
+            gres_splitted_len = len(gres_splitted)
 
-    def _flatten_tres(self, typ, vals):
-        cdef dict out = {}
-        for name, cnt in vals.items():
-            out[f"{typ}{TRES_TYPE_DELIM}{name}"] = cnt
+            name = gres_splitted[0]
+            if gres_splitted_len > 1:
+                typ = gres_splitted[1]
 
-        return out
+            # Check if we have a gres type.
+            if typ.isdigit():
+                cnt = typ
+                typ = None
+            elif gres_splitted_len > 2:
+                cnt = gres_splitted[2]
+            else:
+                # String is somehow malformed, should never happen when the input
+                # comes from the slurmctld. Ignore if it happens.
+                continue
 
-    def to_dict(self, flatten_limits=False, recursive=False):
-        cdef dict inst_dict = instance_to_dict(self, recursive)
+            # Dict Key-Name depends on if we have a gres type or not
+            name_and_typ = f"{name}:{typ}" if typ else name
 
-        if flatten_limits:
-            vals = inst_dict.pop("fs")
-            inst_dict.update(self._flatten_tres("fs", vals))
+            gres = GenericResourceLayout(name, typ, cnt)
 
-            vals = inst_dict.pop("license")
-            inst_dict.update(self._flatten_tres("license", vals))
+            if "IDX" in gres_splitted:
+                # Cover cases with IDX
+                indexes = gres_splitted[3] if not typ else gres_splitted[4]
+                gres.indexes = [int(idx) for idx in indexes.split(",")]
 
-            vals = inst_dict.pop("gres")
-            inst_dict.update(self._flatten_tres("gres", vals))
+            output[name_and_typ] = gres
 
-        return inst_dict
+        return output
 
 
 cdef class TrackableResourceFilter:
@@ -134,27 +168,142 @@ cdef class TrackableResourceFilter:
         self._alloc()
 
 
-cdef class TrackableResources(dict):
+cdef class TrackableResources:
 
-    def __init__(self):
-        pass
+    def __init__(self, global_tres_data=None, **kwargs):
+        self._setup_defaults()
+        self._init_from_dict(kwargs, global_tres_data)
+
+    def _setup_defaults(self):
+        self.fs = FilesystemResources()
+        self.gres = GenericResources()
+        self.license = LicenseResources()
+        self.interconnect = InterconnectResources()
+        self.other = OtherResources()
+        self._id_map = {}
+
+    def _init_from_dict(self, tres_dict, global_tres_data):
+        if not tres_dict:
+            return None
+
+        for k, v in tres_dict.items():
+            typ, name, tid = k, None, None
+
+            if k.isdigit() and global_tres_data:
+                tres_id_map = global_tres_data
+                if isinstance(global_tres_data, TrackableResources):
+                    tres_id_map = global_tres_data._id_map
+
+                global_tres = tres_id_map.get(int(k))
+                if not global_tres:
+                    continue
+
+                typ, name, tid = global_tres.type, global_tres.name, global_tres.id
+            elif TRES_TYPE_DELIM in k:
+                typ, name = self._unflatten_tres(k)
+
+            if isinstance(v, dict):
+                flattened = self._flatten_tres(k, v)
+                self._init_from_dict(flattened, global_tres_data)
+                continue
+
+            # Make sure we actually have a valid value and type
+            tres = TrackableResource.from_type_and_count(typ, v)
+            if not tres:
+                continue
+
+            tres.name = name
+            tres.id = tid
+            self._handle_tres_type(tres)
 
     @staticmethod
-    def load(Connection db_connection=None, name_is_key=True):
-        """Load Trackable Resources from the Database.
+    def from_str(tres_str, global_tres_data=None, on_empty=None):
+        if not tres_str:
+            return on_empty
 
-        Args:
-            name_is_key (bool, optional):
-                By default, the keys in this dict are the names of each TRES.
-                If this is set to `False`, then the unique ID of the TRES will
-                be used as dict keys.
-        """
+        return TrackableResources.from_cstr(tres_str, global_tres_data, on_empty)
+
+    @staticmethod
+    cdef TrackableResources from_cstr(char *tres_str, global_tres_data=None, on_empty=None):
+        cdef:
+            TrackableResources out = TrackableResources.__new__(TrackableResources)
+            dict tres_dict = cstr.to_dict(tres_str)
+
+        if not tres_dict:
+            return on_empty
+
+        out._setup_defaults()
+        out._init_from_dict(tres_dict, global_tres_data)
+        return out
+
+    def _validate(self, tres_data):
+        id_dict = _tres_names_to_ids(self.to_dict(flatten_limits=True),
+                                    tres_data)
+        return id_dict
+
+    def _unflatten_tres(self, type_and_name):
+        typ, name = type_and_name.split(TRES_TYPE_DELIM, 1)
+        return typ, name
+
+    def _flatten_tres(self, typ, vals):
+        cdef dict out = {}
+        for name, val in vals.items():
+            typ = typ or name
+            full_name = f"{typ}{TRES_TYPE_DELIM}{name}"
+            out[full_name] = val
+
+        return out
+
+    def to_dict(self, recursive=False, flatten_limits=False):
+        cdef dict inst_dict = instance_to_dict(self, recursive)
+
+        if flatten_limits:
+            vals = inst_dict.pop("fs")
+            inst_dict.update(self._flatten_tres("fs", vals))
+
+            vals = inst_dict.pop("license")
+            inst_dict.update(self._flatten_tres("license", vals))
+
+            vals = inst_dict.pop("gres")
+            inst_dict.update(self._flatten_tres("gres", vals))
+
+            vals = inst_dict.pop("interconnect")
+            inst_dict.update(self._flatten_tres("interconnect", vals))
+
+            vals = inst_dict.pop("other")
+            inst_dict.update(self._flatten_tres(None, vals))
+
+        return inst_dict
+
+    def _handle_tres_type(self, tres):
+        if tres.type == "gres":
+            gres = tres
+            if tres.name == "gpu" or tres.name.startswith("gpu:"):
+                gres = GPU.from_tres(tres)
+            self.gres[gres.name] = gres
+        elif isinstance(tres, GPU):
+            self.gres[tres.name] = tres
+        elif tres.type == "fs":
+            self.fs[tres.name] = tres
+        elif tres.type == "license":
+            self.license[tres.name] = tres
+        elif tres.type == "interconnect":
+            self.interconnect[tres.name] = tres
+        elif hasattr(self, tres.type):
+            setattr(self, tres.type, tres)
+        elif tres.type:
+            print(tres.type, tres.name, tres.type_and_name)
+            self.other[tres.type_and_name] = tres
+
+    @staticmethod
+    def load(Connection db_connection=None):
+        """Load Trackable Resources from the Database."""
         cdef:
             TrackableResources out = TrackableResources()
             TrackableResource tres
             Connection conn
-            SlurmList tres_data 
-            SlurmListItem tres_ptr 
+            SlurmList tres_data
+            SlurmListItem tres_ptr
             TrackableResourceFilter db_filter = TrackableResourceFilter()
 
         # Prepare SQL Filter
@@ -173,30 +322,10 @@ cdef class TrackableResources(dict):
         for tres_ptr in SlurmList.iter_and_pop(tres_data):
             tres = TrackableResource.from_ptr(
                     <slurmdb_tres_rec_t*>tres_ptr.data)
-            _id = tres.type_and_name if name_is_key else tres.id
-            out[_id] = tres
+            out._handle_tres_type(tres)
+            out._id_map[tres.id] = tres
 
         return out
-
-    @staticmethod
-    cdef TrackableResources from_str(char *tres_str):
-        cdef:
-            TrackableResources tres_collection
-            TrackableResource tres
-            str raw_str = cstr.to_unicode(tres_str)
-            dict tres_dict
-
-        tres_collection = TrackableResources.__new__(TrackableResources)
-        if not raw_str:
-            return tres_collection
-
-        tres_collection.raw_str = raw_str
-        tres_dict = cstr.to_dict(tres_str)
-        for tres_id, val in tres_dict.items():
-            tres = TrackableResource(tres_id)
-            tres.ptr.count = val
-
-        return tres
 
     @staticmethod
     cdef find_count_in_str(char *tres_str, typ, on_noval=0, on_inf=0):
@@ -205,47 +334,22 @@ cdef class TrackableResources(dict):
 
 cdef class TrackableResource:
 
-    def __cinit__(self):
-        self.ptr = NULL
-
-    def __init__(self, tres_id):
-        self._alloc_impl()
-        self.ptr.id = tres_id
-
-    def __dealloc__(self):
-        self._dealloc_impl()
-
-    def _alloc_impl(self):
-        if not self.ptr:
-            self.ptr = <slurmdb_tres_rec_t*>try_xmalloc(
-                    sizeof(slurmdb_tres_rec_t))
-            if not self.ptr:
-                raise MemoryError("xmalloc failed for slurmdb_tres_rec_t")
-
-    def _dealloc_impl(self):
-        slurmdb_destroy_tres_rec(self.ptr)
-        self.ptr = NULL
+    def __init__(self, tres_type, count, name=None, tres_id=None):
+        self.type = tres_type
+        self.id = tres_id
+        self.name = name
+        self.count = int(count)
+        # rec_count
+        # alloc_secs
 
     @staticmethod
-    cdef TrackableResource from_ptr(slurmdb_tres_rec_t *in_ptr):
-        cdef TrackableResource wrap = TrackableResource.__new__(TrackableResource)
-        wrap.ptr = in_ptr
-        return wrap
-
-    def to_dict(self):
-        return instance_to_dict(self)
-
-    @property
-    def id(self):
-        return self.ptr.id
-
-    @property
-    def name(self):
-        return cstr.to_unicode(self.ptr.name)
-
-    @property
-    def type(self):
-        return cstr.to_unicode(self.ptr.type)
+    cdef TrackableResource from_ptr(slurmdb_tres_rec_t *ptr):
+        return TrackableResource(
+            tres_type = cstr.to_unicode(ptr.type),
+            count = u64_parse(ptr.count, on_noval=1),
+            name = cstr.to_unicode(ptr.name),
+            tres_id = int(ptr.id),
+        )
 
     @property
     def type_and_name(self):
@@ -255,14 +359,55 @@ cdef class TrackableResource:
 
         return type_and_name
 
-    @property
-    def count(self):
-        return u64_parse(self.ptr.count)
     def to_dict(self, recursive = False):
         return instance_to_dict(self, recursive)
 
-    # rec_count
-    # alloc_secs
+    @staticmethod
+    def from_type_and_count(tres_type, count):
+        if isinstance(tres_type, str) and tres_type.isdigit():
+            return None
+
+        if tres_type == "mem":
+            count = dehumanize(count)
+        else:
+            count = int(count)
+
+        if count == slurm.NO_VAL64:
+            return None
+
+        return TrackableResource(tres_type=tres_type, count=count)
+
+
+cdef class GPU:
+
+    def __init__(self, count, gpu_type=None, tres_id=None):
+        self.count = int(count)
+        self.type = gpu_type
+        self.id = tres_id
+
+    def to_dict(self, recursive = False):
+        return instance_to_dict(self, recursive)
+
+    @staticmethod
+    def from_tres(tres):
+        if ":" in tres.name:
+            _, typ = tres.name.split(":")
+        else:
+            typ = None
+
+        return GPU(
+            count = tres.count,
+            gpu_type = typ,
+            tres_id = tres.id,
+        )
+
+    @property
+    def name(self):
+        return f"gpu:{self.type}" if self.type else "gpu"
+
+    @property
+    def type_and_name(self):
+        return f"gres{TRES_TYPE_DELIM}{self.name}"
 
 
 cdef find_tres_count(char *tres_str, typ, on_noval=0, on_inf=0):
@@ -295,7 +440,7 @@ cdef merge_tres_str(char **tres_str, typ, val):
     cstr.from_dict(tres_str, current)
 
 
-cdef _tres_ids_to_names(char *tres_str, TrackableResources tres_data):
+cdef _tres_ids_to_names(char *tres_str, dict tres_id_map):
     if not tres_str:
         return None
 
@@ -303,40 +448,46 @@ cdef _tres_ids_to_names(char *tres_str, TrackableResources tres_data):
         dict tdict = cstr.to_dict(tres_str)
         list out = []
 
-    if not tres_data:
+    if not tres_id_map:
         return None
 
     for tid, cnt in tdict.items():
         if isinstance(tid, str) and tid.isdigit():
             _tid = int(tid)
-            if _tid in tres_data:
+            if _tid in tres_id_map:
                 out.append(
-                    (tres_data[_tid].type, tres_data[_tid].name, int(cnt))
+                    (tres_id_map[_tid].type, tres_id_map[_tid].name, int(cnt))
                 )
 
     return out
 
 
-def _tres_names_to_ids(dict tres_dict, TrackableResources tres_data):
+def _tres_names_to_ids(dict tres_dict, tres_data):
     cdef dict out = {}
     if not tres_dict:
         return out
 
-    for tid, cnt in tres_dict.items():
-        real_id = _validate_tres_single(tid, tres_data)
-        out[real_id] = cnt
+    tres_id_map = tres_data
+    if isinstance(tres_data, TrackableResources):
+        tres_id_map = tres_data._id_map
+
+    for tres in tres_dict.values():
+        if not tres:
+            continue
+
+        real_id = _validate_tres_single(tres, tres_id_map)
+        out[real_id] = tres.count
 
     return out
 
 
-def _validate_tres_single(tid, TrackableResources tres_data):
-    for tres in tres_data.values():
-        if tid == tres.id or tid == tres.type_and_name:
-            return tres.id
+def _validate_tres_single(local_tres, dict tres_id_map):
+    for global_tres in tres_id_map.values():
+        if local_tres.id == global_tres.id or local_tres.type_and_name == global_tres.type_and_name:
+            return global_tres.id
 
-    raise ValueError(f"Invalid TRES specified: {tid}")
+    raise ValueError(f"Invalid TRES specified: {local_tres.type_and_name}")
 
 
-cdef _set_tres_limits(char **dest, TrackableResourceLimits src,
-                          TrackableResources tres_data):
+cdef _set_tres_limits(char **dest, src, tres_data):
     cstr.from_dict(dest, src._validate(tres_data))
